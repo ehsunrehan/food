aa1a95fbb2ff6b8237dab3a3bbe3442bda78fa4a
Ehsun Rehan <ehsunredmi@gmail.com>
2026-06-16 13:28:32 +0500

    this one

--- js/main.js
+++ js/main.js
@@ -1,25 +1,37 @@
 const menuBtn = document.getElementById("menu-btn");
-
 const sidebar = document.getElementById("sidebar");
-
 const closeBtn = document.getElementById("close-btn");
 
+// sidebar
 if(menuBtn){
-
     menuBtn.addEventListener("click",()=>{
-
         sidebar.style.right="0";
-
     });
-
 }
 
 if(closeBtn){
-
     closeBtn.addEventListener("click",()=>{
-
         sidebar.style.right="-320px";
-
     });
+}
 
+
+// =======================
+// FAKE AR SYSTEM
+// =======================
+
+const arBtn = document.getElementById("ar-btn");
+const fakeAR = document.getElementById("fake-ar");
+const closeAR = document.getElementById("close-ar");
+
+if(arBtn){
+    arBtn.addEventListener("click",()=>{
+        fakeAR.classList.remove("hidden");
+    });
+}
+
+if(closeAR){
+    closeAR.addEventListener("click",()=>{
+        fakeAR.classList.add("hidden");
+    });
 }
